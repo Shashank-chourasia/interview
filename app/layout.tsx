0f41315da95d959b293dc7edab1f5132f3c04788
Shashank <shashankchourasia2@gmail.com>
2025-04-04 23:11:58 +0530

    shashank chourasia

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
+
 import "./globals.css";
-import { Toaster } from "sonner";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -9,8 +10,8 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Interview",
-  description: "An AI based platform for preparing mock interview.",
+  title: "PrepWise",
+  description: "An AI-powered platform for preparing for mock interviews",
 };
 
 export default function RootLayout({
@@ -20,10 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`${monaSans.className} antialiased pattern`}
-      >
+      <body className={`${monaSans.className} antialiased pattern`}>
         {children}
+
         <Toaster />
       </body>
     </html>
