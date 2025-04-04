2aab544b734c1d65053d32326f8c663c5ee768cb
Shashank <shashankchourasia2@gmail.com>
2025-04-04 13:32:50 +0530

    shashank chourasia

---
+++ firebase/client.ts
@@ -0,0 +1,24 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRXbC_qEVpfAQl5zHtVrHPw9DNrvPqgPk",
+  authDomain: "interview-1ca1f.firebaseapp.com",
+  projectId: "interview-1ca1f",
+  storageBucket: "interview-1ca1f.firebasestorage.app",
+  messagingSenderId: "679534900372",
+  appId: "1:679534900372:web:abbd65ff5d7ce6b567f081",
+  measurementId: "G-VP1Y5691FS"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
